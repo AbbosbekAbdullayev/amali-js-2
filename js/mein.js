39b15c22752a050c06e2266aee196f3a39b5f505
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-25 20:01:29 +0500

    math-amal

--- js/mein.js
+++ js/mein.js
@@ -1,5 +1,12 @@
 var age = prompt("qo'ylaringiz soni");
 // age=Number(age);
 // age=age-0;
-console.log(age);
+console.log(age );
+console.log(5+2);
+console.log(5-2);
+console.log(5*2);
+console.log(5**2);
+
+
+
 
