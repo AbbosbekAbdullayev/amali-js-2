2192aa6b8d0d1df2cad754a4c4069c3b468643e8
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-25 20:18:30 +0500

    shugil varyand manfiyligi

--- js/mein.js
+++ js/mein.js
@@ -1,19 +1,21 @@
-var age = prompt("qo'ylaringiz soni");
+var son = prompt("qo'ylaringiz soni");
 // age=Number(age);
 // age=age-0;
-console.log(age );
-console.log(5+2);
-console.log(5-2);
-console.log(5*2);
-console.log(5**2);
-console.log(5%3);
-console.log(1+1+1+1+1+2+1616+5);
 ++son;
 ++son;
 ++son;
-
-
-
+--son;
+--son;
+--son;
+--son;
+--son;
+--son;
+--son;
+--son;
+--son;
+--son;
+--son;
+console.log(son);
 
 
 
