2563ab03a1bf44e3e21e4eefff76a65db06f4b44
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-28 18:46:24 +0500

    math

--- js/mein.js
+++ js/mein.js
@@ -1,22 +1,24 @@
-var son = prompt("qo'ylaringiz soni");
-son=Number(son);
-// age=age-0;
-++son;
-++son;
-++son;
---son;
---son;
---son;
---son;
---son;
---son;
---son;
---son;
---son;
---son;
---son;
-console.log(typeof son);
+// var son = prompt("qo'ylaringiz soni");
+// // son=Number(son);
+// // age=age-0;
+// ++son;
+// ++son;
+// ++son;
+// --son;
+// --son;
+// --son;
+// --son;
+// --son;
+// --son;
+// --son;
+// --son;
+// --son;
+// --son;
+// --son;
+// console.log(typeof 15);
 
-
+let matematik
+matematik = Math.ceil(10.2);
+console.log(matematik);
 
 
