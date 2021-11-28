ca2bc4b36e59b56b78b9e2c628b33365d4b9a511
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-28 20:50:34 +0500

    fewf

--- js/mein.js
+++ js/mein.js
@@ -30,7 +30,7 @@
 
 qabaassscssdscdcdcscscscdscdcds
 cxcdscdccdzcdxzxzxzscdsdscdscdscdc
-dscdsddsasadasssadsadcdscdsdaaaadccdscdscdscdscdscdscdscdscdc
+dscdsddsasadfffdasssadsadcdscdsdaaaadccdscdscdscdscdscdscdscdscdc
   sadssasadsacdsdscdsccdscsscds
 
 
