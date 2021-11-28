4e8a341a876da6093f69ac8de8a1b677a853f808
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-28 20:41:32 +0500

    namber q

--- js/mein.js
+++ js/mein.js
@@ -22,11 +22,13 @@
 //son = Math.floor(10.2);-pasga yaxlitlaydi
 //son = Math.round(10.4); haqiyqi taqriban yaxlitlash
 
-son =Math.floor(Math.random()*5)
+// son =Math.floor(Math.random()*5)
 
-let arr = ['A','b', 'c', 'd','e'];
-console.log(arr[son]+son*100000)+5
-// console.log(son);
+// let arr = ['A','b', 'c', 'd','e'];
+// console.log(arr[son]+son*100000)+5
+// // console.log(son);
+
+q
 
 
   
