6d46ff7859bd257c37f78418697569da97108104
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-28 20:50:09 +0500

    dsad

--- js/mein.js
+++ js/mein.js
@@ -30,7 +30,7 @@
 
 qabaassscssdscdcdcscscscdscdcds
 cxcdscdccdzcdxzxzxzscdsdscdscdscdc
-dscdsdcdscdsdaadccdscdscdscdscdscdscdscdscdc
+dscdsdcdscdsdaaaadccdscdscdscdscdscdscdscdscdc
   sadcdsdscdsccdscsscds
 
 
